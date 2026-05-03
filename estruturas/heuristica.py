80dc77db06f08fb5892f28749fe076bdd9aa5ad2
Caio Macedo <caiomacedo123@outlook.com>
2026-05-03 14:28:16 -0300

    adicionando constante_bonus em heurísitica para facilitar testes

--- estruturas/heuristica.py
+++ estruturas/heuristica.py
@@ -1,4 +1,5 @@
 # Heurística dinâmica que ajusta a estimativa com base na proximidade de recompensas disponíveis.
+CONSTANTE_BONUS = 15
 def heuristica_dinamica(atual, destino, recompensas_disponiveis):
     distancia_objetivo = abs(atual[0] - destino[0]) + abs(atual[1] - destino[1])
     bonus_atracao = 0
@@ -7,6 +8,6 @@ def heuristica_dinamica(atual, destino, recompensas_disponiveis):
         distancias_recompensas = [abs(atual[0] - r[0]) + abs(atual[1] - r[1]) for r in recompensas_disponiveis]
         dist_prox = min(distancias_recompensas)
         if dist_prox <= 6:
-            bonus_atracao = 15 / (dist_prox + 1)
+            bonus_atracao = CONSTANTE_BONUS / (dist_prox + 1)
             
     return max(0, distancia_objetivo - bonus_atracao)
